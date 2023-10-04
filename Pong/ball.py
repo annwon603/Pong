bc93a655c62a90a98b28e9cd147d83e33b10922c
annwon603 <101071975+annwon603@users.noreply.github.com>
2023-10-03 21:34:49 -0700

    Created Pong
    1. Created a ball class which changes it's velocity after bouncing off the wall
    2. Created a main class which updates the sprites, runs the game, control the keys, changes the ball's velocity, updates the score , display the game

--- Pong/ball.py
+++ Pong/ball.py
@@ -1,11 +1,11 @@
 # Importing the pygame library
 import pygame
-
+from random import randint
 BLACK = (0 , 0 , 0)
 
 class Ball(pygame.sprite.Sprite):
     
-    def __initi__(self, color, width, height):
+    def __init__(self, color, width, height):
         
         super().__init__()
         #creating a hit box for the ball
@@ -15,8 +15,17 @@ class Ball(pygame.sprite.Sprite):
         #creating a drawing on top of the hitbox
         pygame.draw.rect(self.image, color, [0, 0, width, height])
         
+        self.velocity = [randint(4,8),randint(-8,8)]
+
         self.rect = self.image.get_rect()
+    
+    def update(self):
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
         
+    def bounce(self):
+        self.velocity[0] = -self.velocity[0]
+        self.velocity[1] = randint(-8,8)
     
         
     
--- Pong/main.py
+++ Pong/main.py
@@ -3,7 +3,8 @@ import pygame
 
 #importing the Paddle class from paddle.py
 from paddle import Paddle
-
+#importing the Ball class from ball.py
+from ball import Ball
 # initializing the pygame library
 pygame.init()
 
@@ -12,6 +13,7 @@ NAVY_BLUE = (32 , 42 , 68)
 WHITE = (255, 255, 255)
 
 #size of the game window
+#700 is width,500 is height
 size = (700 , 500)
 #created the screen
 game_window = pygame.display.set_mode(size)
@@ -28,14 +30,22 @@ paddle2 = Paddle(WHITE, 10, 100)
 paddle2.rect.x = 670
 paddle2.rect.y = 200
 
+ball = Ball(WHITE,10,10)
+ball.rect.x=345
+ball.rect.y=245
+
 sprite_list = pygame.sprite.Group()
 sprite_list.add(paddle1)
 sprite_list.add(paddle2)
+sprite_list.add(ball)
 
 run_game = True
 # in-game clock to keep track of time intervals
 # counts every ms, 1000 ms= 1 sec
 clock = pygame.time.Clock()
+
+score_player_1 = 0
+score_player_2 = 0
 
 while run_game:
     for event in pygame.event.get():
@@ -56,12 +66,34 @@ while run_game:
     
     sprite_list.update()
     
+    if ball.rect.x >= 690:
+        ball.velocity[0] = -ball.velocity[0]
+        score_player_1 += 1
+    if ball.rect.x <= 0:
+        ball.velocity[0] = -ball.velocity[0]
+        score_player_2 += 1
+    if ball.rect.y >= 490:
+        ball.velocity[1] = -ball.velocity[1]
+    if ball.rect.y <= 0:
+        ball.velocity[1] = -ball.velocity[1]    
+    
+    if pygame.sprite.collide_mask(ball, paddle1) or pygame.sprite.collide_mask(ball, paddle2):
+        ball.bounce()
+    
+    
     #things I want to be consistent throughtout the game
     game_window.fill(NAVY_BLUE)
     #drawing the net in the middle       [top]     [bottom]  [width]
     pygame.draw.line(game_window, WHITE, [349, 0], [349, 500], 5)
     
     sprite_list.draw(game_window)
+    
+    font = pygame.font.Font(None, 75)
+    text = font.render(str(score_player_1), 1, WHITE)
+    game_window.blit(text, (250,10) )
+    text = font.render(str(score_player_2), 1, WHITE)
+    game_window.blit(text, (420,10) )
+    
     #updates the screen
     pygame.display.flip()
     #clock is ticking 60x per second
